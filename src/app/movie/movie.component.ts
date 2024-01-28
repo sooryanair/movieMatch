147cd63156512f86d38351cf1e442c6f076b0aad
sooryanair <sooryaqw@gmail.com>
2024-01-28 19:19:51 +0530

    removal of swiper

--- src/app/movie/movie.component.ts
+++ src/app/movie/movie.component.ts
@@ -2,50 +2,51 @@ import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
 import { ApiService } from '../services/api.service';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-movie',
   standalone: true,
-  imports: [CommonModule, ButtonModule],
+  imports: [CommonModule, ButtonModule, CardModule],
   templateUrl: './movie.component.html',
   styleUrl: './movie.component.css',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
+
 export class MovieComponent implements OnInit {
   movieList: any;
-  swiperEl: any;
+  currentMovie: any;
+  position: number = 0;
+  likeList: any[] = [];
 
-  constructor(private apiService: ApiService) {}
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
-    this.swiperEl = document.querySelector('swiper-container');
     this.apiService.getMovies().subscribe((res) => {
       this.movieList = res.results;
+      this.currentMovie = this.movieList[this.position];
       console.log(res);
     });
-    this.onLike();
-    this.onDislike();
   }
 
   // SOLUTION - go to next movie no matter which direction we swipe
 
-  onLike() {
-    this.swiperEl.swiper.on('slideNextTransitionEnd', function () {});
-  }
-
-  onDislike() {
-    this.swiperEl.swiper.on('slidePrevTransitionEnd', () => {
-      this.dislike();
-    });
-  }
-
   like() {
-    this.swiperEl.swiper.slideNext();
+    this.nextMovie();
+    //check if already exists in the array before pushing it
+    this.likeList.push(this.currentMovie);
+    console.log(this.likeList)
     // we add to likes
   }
 
   dislike() {
-    this.swiperEl.swiper.slideNext();
+    this.nextMovie();
     // remove from movie list
+  }
+
+  private nextMovie() {
+    this.position++;
+    if (this.movieList.length > this.position) {
+      this.currentMovie = this.movieList[this.position];
+    }
   }
 }
